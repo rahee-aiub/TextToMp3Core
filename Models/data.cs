84f02dbb6c0eb3711b5f3ad944f7edef82af1e73
Rahee <rahee.pm@gmail.com>
2021-10-25 10:12:27 +0600

    Project Initial

---
+++ Models/data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TextToSpeech.Models
+{
+    public class data
+    {
+            public string txtData { get; set; }
+    }
+}
